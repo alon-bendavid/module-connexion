6eba4cdb6faa94fb014e22159fff49ae255fd564
alon-bendavid <ben-david.alon@laplateforme.io>
2022-11-29 20:47:31 +0100

    sign in logic start

--- inclueds/myadmin.php
+++ inclueds/myadmin.php
@@ -9,25 +9,37 @@ $request = $mysqli->query("SELECT * from utilisateurs");
 $usernames = [];
 while (($fetched_table = $request->fetch_array())  != 0) {
     $usernames[] =   $fetched_table[1];
-    // print_r($fetched_table);
+    // var_dump($fetched_table[1]);
     // print_r($fetched_table[1]);
 }
-
+//check if username already exist
 $exiest = true;
 if (isset($_POST['submit'])) {
     foreach ($usernames as $user => $x) {
         if ($_POST['username'] == $x) {
             echo " username already exists please choose another name";
-            die();
+            $exiest = true;
         } else {
             $exiest = false;
         }
     }
+    //pass check
+    $passCheck = 0;
+}
+if (isset($_POST['submit'])) {
+    if ($_POST['repass']  !== $_POST['password']) {
+        echo "password doesnt match, please retype password";
+        die();
+    } elseif ($_POST['password'] === $_POST['repass']) {
+
+        $passCheck = true;
+    }
 }
 
+//singing up the user into the database
+if (isset($_POST['submit']) &&  $exiest == 0 && $passCheck = true) {
 
-if (isset($_POST['submit']) &&  $exiest == false) {
-
+    // var_dump($passCheck);
 
     $username = $_POST['username'];
     $name = $_POST['name'];
@@ -44,9 +56,11 @@ if (isset($_POST['submit']) &&  $exiest == false) {
     }
     mysqli_stmt_bind_param($stmt, "ssss", $username, $name, $Lname, $pwd);
     mysqli_stmt_execute($stmt);
-    // header('Location: ' . 'connexion.php');
-
+    header('Location: ' . 'connexion.php');
 }
+
+
+    
 
 // $sql = "INSERT INTO utilisateurs (`id`, `login`, `prenom`, `nom`, `password`) VALUES (NULL,$username,$name, $Lname,$pwd)";
 
--- pages/connexion.php
+++ pages/connexion.php
@@ -1,12 +1,20 @@
 <?php
 include('header.php');
-
+include('../inclueds/myadmin.php');
 
 ?>
 
 <body>
     <h1>sign in</h1>
+    <form action="login.php" method="post">
+        <input type="text" placeholder="username" name="login"><br required>
 
+        <input type="text" placeholder="password" name="loginPwd" required><br>
+
+        <button type="submit" name="submit">Sign</button>
+
+
+    </form>
 
 </body>
 
